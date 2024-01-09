784b6fb99412de74c197c8a7a2d43c4aea9b40ca
James Stanger <jamesstanger987@gmail.com>
2024-01-08 23:13:19 -0800

    Added some styles to the nav

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -4,15 +4,36 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <nav>
-    <RouterLink to="/">Logo</RouterLink>
+    <RouterLink to="/"><img src="../assets/logo.png" alt="Logo" /></RouterLink>
 
     <RouterLink to="/meet-jared">Meet Jared</RouterLink>
+    <span class="jeb-nav--spacer" />
     <RouterLink to="/books">Books</RouterLink>
+    <span class="jeb-nav--spacer" />
     <RouterLink to="/newsletter">Newsletter</RouterLink>
+    <span class="jeb-nav--spacer" />
     <RouterLink to="/educators">Educators</RouterLink>
+    <span class="jeb-nav--spacer" />
     <RouterLink to="/extras">Extras</RouterLink>
   </nav>
 </template>
 
 <style scoped>
+  img {
+    width: 30vw;
+  }
+
+  nav {
+    width: 60%;
+    display: grid;
+    grid-auto-flow: column;
+    align-items: center;
+  }
+
+  .jeb-nav--spacer {
+    width: 1rem;
+    height: 1rem;
+    border-radius: 1rem;
+    background: var(--main-font-color);
+  }
 </style>
